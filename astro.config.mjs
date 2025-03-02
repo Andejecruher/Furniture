c74d558335e30f8df625ceeeb2e74bed57220cc4
Andejecruher <antonio.cruz@clupp.com.mx>
2025-03-01 19:02:02 -0600

    feat: :sparkles: Configurar alias de rutas y actualizar fuentes Eudoxus Sans en estilos globales

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,19 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 // https://astro.build/config
-export default defineConfig({ vite: { plugins: [tailwindcss()] } });
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+    vite: {
+        plugins: [tailwindcss()],
+        resolve: {
+            alias: {
+                '@': resolve(__dirname, 'src')
+            }
+        }
+    }
+});
